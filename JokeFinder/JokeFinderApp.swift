dc9c77568a48c10b02535f97843b73695b7a3d09
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-04-02 19:06:11 -0400

    Initial Commit

---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by Ahmet Kaan Caliskan on 2025-04-02.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
